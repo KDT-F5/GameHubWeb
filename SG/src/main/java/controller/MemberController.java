b9fc929e35ad00509db350c130c9b1de30579016
Ju-S <wntjd0643@gmail.com>
2025-08-25 14:39:29 +0900

    Controller URI에 따른 분기문 작성완료.

--- SG/src/main/java/controller/MemberController.java
+++ SG/src/main/java/controller/MemberController.java
@@ -1,18 +1,42 @@
 package controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/MemberController")
+@WebServlet("*.member")
 public class MemberController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cmd = request.getRequestURI();
+
+        switch(cmd) {
+            case "register.member":{
+                //회원가입
+                break;
+            }
+            case "login.member":{
+                //로그인
+                break;
+            }
+            case "logout.member":{
+                //로그아웃
+                break;
+            }
+            case "update.member":{
+                //업데이트
+                break;
+            }
+            case "delete.member":{
+                //회원탈퇴
+                break;
+            }
+        }
     }
 
     @Override
