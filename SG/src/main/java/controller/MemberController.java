a309f08cb7a8b14c62c123a6b0d2d6b8d476ba02
gijun123 <tteenn1@naver.com>
2025-08-25 15:28:49 +0900

    SHA-256 추가된 MemberController

--- SG/src/main/java/controller/MemberController.java
+++ SG/src/main/java/controller/MemberController.java
@@ -7,10 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet("*.member")
 public class MemberController extends HttpServlet {
+    public static String encrypt(String text) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); // SHA-512은 암호화 글자수 더 작음
+            byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
+            byte[] digest = md.digest(bytes);
 
+            StringBuilder builder = new StringBuilder();
+            for (byte b : digest) {
+                builder.append(String.format("%02x", b));
+            }
+            return builder.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 암호화 실패", e);
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String cmd = request.getRequestURI();
@@ -22,6 +40,12 @@ public class MemberController extends HttpServlet {
             }
             case "login.member": {
                 //로그인
+                String id = request.getParameter("id");
+                String pw = request.getParameter("pw");
+                String lockPw = request.getParameter("lockPw");
+
+
+
                 break;
             }
             case "logout.member": {
