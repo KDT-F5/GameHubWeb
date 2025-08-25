46b8a8d896068d10a83887d3cddecc053bb5c903
pwrmin <pwrmin@naver.com>
2025-08-25 16:20:31 +0900

    회원탈퇴 및 에러페이지 생성 + 파라미터 오류변경

--- SG/src/main/java/controller/MemberController.java
+++ SG/src/main/java/controller/MemberController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import dao.MemberDAO;
+import dto.MemberDTO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("*.member")
 public class MemberController extends HttpServlet {
@@ -17,69 +19,69 @@ public class MemberController extends HttpServlet {
         String cmd = request.getRequestURI();
         MemberDAO dao = MemberDAO.getInstance();
 
-        try{
-        switch(cmd) {
-            case "register.member":{
-                //회원가입
-                String id = request.getParameter("id");
-                String pw = SecurityUtil.encrypt(request.getParameter("pw"));
-                String name = request.getParameter("name");
-                String phonenum = request.getParameter("phone");
-                String email = request.getParameter("email");
-                String zoneCode = request.getParameter("zipcode");
-                String address1 = request.getParameter("address1");
-                String address2 = request.getParameter("address2");
+        try {
+            switch (cmd) {
+                case "register.member": {
+                    //회원가입
+                    String id = request.getParameter("id");
+                    String pw = SecurityUtil.encrypt(request.getParameter("pw"));
+                    String name = request.getParameter("name");
+                    String phonenum = request.getParameter("phone");
+                    String email = request.getParameter("email");
+                    String zoneCode = request.getParameter("zipcode");
+                    String address1 = request.getParameter("address1");
+                    String address2 = request.getParameter("address2");
 
-                MemberDTO temp = new MemberDTO(id, pw, name, phonenum, email, zoneCode, address1, address2, null);
-                dao.insertAll(temp);
+                    MemberDTO temp = new MemberDTO(id, pw, name, phonenum, email, zoneCode, address1, address2, null);
+                    dao.insertAll(temp);
 
 
-                response.sendRedirect("/index.jsp");
-                break;
+                    response.sendRedirect("/index.jsp");
+                    break;
+                }
+                case "login.member": {
+                    //로그인
+                    String id = request.getParameter("id");
+                    String pw = request.getParameter("pw");
+                    String lockPw = request.getParameter("lockPw");
+                    boolean result;
+
+
+                    break;
+                }
+                case "logout.member": {
+                    //로그아웃
+                    request.getSession().setAttribute("loginId", null);
+                    response.sendRedirect("/");
+                    break;
+                }
+                case "update.member": {
+                    //업데이트
+                    break;
+                }
+                case "delete.member": {
+                    //회원탈퇴
+                    String id = request.getParameter("id");
+                    int del_id = dao.delete_assign(id);
+
+                    response.sendRedirect("/webapp/error.jsp");
+                    break;
+                }
+                case "/idcheck.member": {
+                    // 아이디 중복 체크 확인
+                    String id = request.getParameter("id");
+
+                    //ajax로 받기
+                    String result = String.valueOf(dao.isDupli(id));
+                    PrintWriter pw = response.getWriter();
+                    pw.append(result); // 스트링값 true,false 보내줌
+                }
             }
-            case "login.member":{
-                //로그인
-                String id = request.getParameter("id");
-                String pw = request.getParameter("pw");
-                String lockPw = request.getParameter("lockPw");
-                boolean result;
 
-
-                break;
-            }
-            case "logout.member":{
-                //로그아웃
-                request.getSession().setAttribute("loginId", null);
-                response.sendRedirect("/");
-                break;
-            }
-            case "update.member":{
-                //업데이트
-                break;
-            }
-            case "delete.member":{
-                //회원탈퇴
-                String id = request.getParameter("id");
-                int del_id = dao.delete_assign(id);
-
-                response.sendRedirect("/webapp/error.jsp");
-                break;
-            }
-            case "/idcheck.member":{
-                // 아이디 중복 체크 확인
-                String id = request.getParameter("id");
-
-                //ajax로 받기
-                String result = String.valueOf(dao.isDupli(id));
-                PrintWriter pw = response.getWriter();
-                pw.append(result); // 스트링값 true,false 보내줌
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
